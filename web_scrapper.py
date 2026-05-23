40eb0728f2effe78dcf4982fe4734506c9ed6713
Pragya Dhungel <pragyadhungel2006@gmail.com>
2026-05-23 09:26:33 +0545

    Refactor title scraping into fetch_title function

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,17 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-with open ('output.csv',mode='w',newline='') as file:
-    writer = csv.writer(file)
-    url = "https://books.toscrape.com"
-    response = requests.get(url)
-    parse = BeautifulSoup(response.text, 'html.parser')
-    article_tags = parse.find_all("article", attrs = {"class":"product_pod"})
+
+def fetch_title():
+    
+    article_tags = soup.find_all("article", attrs = {"class":"product_pod"})
     for block in article_tags:
         a_tags = block.find_all("a")
         required_a = a_tags[1]
         title = required_a["title"]
+        
         writer.writerow([title])
+    return(title)
+       
+         
+with open ('output.csv',mode='w',newline='') as file:
+    writer = csv.writer(file)
+    header = ['Title', 'Price']    
+    writer.writerow(header)
+    url = "https://books.toscrape.com"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    fetch_title()
+    
+    
+ 
+    
+    
+   
+        
         
 
     
